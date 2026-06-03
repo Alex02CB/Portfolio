6ed4ac6f641dd1b584569c749a470b4d40d8e30d
Alex02CB <131156256+Alex02CB@users.noreply.github.com>
2026-06-03 23:13:30 +0200

    Add files via upload

---
+++ src/components/DownloadCVLink.jsx
@@ -0,0 +1,11 @@
+import '../index.css'
+
+const DownloadCVLink = () => {
+  return (
+    <a href="/CV-Juan-Alejandro-Campillo-Bustamante.pdf" download className='downloadCVLink'>
+      Descargar CV
+    </a>
+  );
+};
+
+export default DownloadCVLink;
